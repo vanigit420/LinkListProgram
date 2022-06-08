9c6bb12f4bc976284c908ad1541628c57c9c77d3
shivani korwan <shivanikorwan63232@gmail.com>
2022-06-08 22:25:11 +0530

    Add Method

---
+++ LinkListDemo/LinkListDemo/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class Node
+    {
+        internal static Node temp;
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ LinkListDemo/LinkListDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LinkedListDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the linked list");
+
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+
+            list.Display();
+
+            List<Node> nodes = new List<Node>();
+
+        }
+    }
+}
